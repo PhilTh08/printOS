9cf1a868b5d5cb812ce3c6c84935148e1f76eb2e
Philipp <philipp.thaeter@gmail.com>
2026-07-19 05:09:13 +0200

    PWA Manifest purpose korrigiert

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,19 +12,23 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "any",
     background_color: "#080c0f",
     theme_color: "#080c0f",
-    categories: ["business", "productivity", "utilities"],
+    categories: [
+      "business",
+      "productivity",
+      "utilities",
+    ],
     icons: [
       {
         src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
